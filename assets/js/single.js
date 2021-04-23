1b702b2ed2e924a890e22804ddba1d50cc445b6a
Rogelio <rogeliogalicia@Rogelios-Air.attlocal.net>
2021-04-22 22:38:50 -0700

    handle errors

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,9 +1,26 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function() {
+    // grab repo name from url query string 
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    if (repoName) {
+        // display repo name on the page
+        repoNameEl.textContent = repoName;
+
+        getRepoIssues(repoName);
+    } else {
+        // if no repo was given, redirect to the homepage
+        document.location.replace("./index.html");
+    }
+};
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
+    // make a get request to url
     fetch(apiUrl).then(function(response) {
         // request was succesful
         if (response.ok) {
@@ -18,7 +35,8 @@ var getRepoIssues = function(repo) {
             });
         }
         else {
-            alert("There was a problem with your request!");
+            // if not successful, redirect to homepage
+            document.location.replace("./index.html");
         }
     });
 };
@@ -34,8 +52,6 @@ var displayIssues = (function(issues) {
         issueEl.classList = "list-element flex-row justify-space-between align-center";
         issueEl.setAttribute("href", issues[i].html_url);
         issueEl.setAttribute("target", "_blank");
-       
-    //}
 
         // create span to hold issue title 
         var titleEl = document.createElement("span");
@@ -58,6 +74,7 @@ var displayIssues = (function(issues) {
         issueEl.appendChild(typeEl);
 
         issueContainerEl.appendChild(issueEl);
+
     }
 });
 
@@ -73,4 +90,4 @@ var displayWarning = function(repo) {
     // append to warning container
     limitWarningEl.appendChild(linkEl);
 };
-getRepoIssues("expressjs/express");
+getRepoName()
